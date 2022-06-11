b00bfdf289e1c79cdc5284148e787c762ec60179
cathyzhang999 <CathyStudy999@outlook.com>
2022-06-11 21:15:23 +0800

    Add files via upload

---
+++ create_dir.py
@@ -0,0 +1,12 @@
+import os
+
+a = os.path.exists('abc')
+b = os.getcwd()            #取得当前目录
+os.mkdir('d:\\jianyan')    #一级文件夹
+
+
+import os
+
+a = os.path.exists('abc')
+b = os.getcwd()
+os.makekdirs('d:\\a\\jianyan')  #多级文件夹
